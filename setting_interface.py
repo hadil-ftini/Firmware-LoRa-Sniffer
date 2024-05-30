a276273c88bb013a0842a951c13e0ae22603f2e4
hadil-ftini <hadylftini@gmail.com>
2024-05-30 01:59:35 +0100

    rotation du hmi

--- setting_interface.py
+++ setting_interface.py
@@ -9,12 +9,15 @@ class TestInterface(QWidget):
 
     def __init__(self, previous_window):
         super().__init__()
-        self.rotate(90)
+        
         self.setWindowTitle("Setting Test Interface")
         self.setGeometry(100, 100, 400, 300)
         # Adjusting geometry for a 2.8 inch screen
         self.setGeometry(0, 0, 800, 480)
-        
+        transform = QTransform().rotate(90)
+        rotated_pixmap = pixmap.transformed(transform, Qt.SmoothTransformation)
+        self.logo_label.setPixmap(rotated_pixmap)
+        self.logo_label.setFixedSize(rotated_pixmap.size())
         # Logo
         self.logo_label = QLabel(self)
         pixmap = QPixmap('enterprise_logo.png')
